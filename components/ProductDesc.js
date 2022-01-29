79691996d5070fc8f985c7e173c2394c0fb2031d
andi <saputro.andi593@gmail.com>
2022-01-29 23:38:50 +0700

    fix deployment error

--- components/ProductDesc.js
+++ components/ProductDesc.js
@@ -15,7 +15,7 @@ function ProductDesc() {
         <div className="product-details">
           <p>
             These low-profile sneakers are your perfect casual wear companion.
-            Featuring a durable rubber outer sole, they'll withstand everything
+            Featuring a durable rubber outer sole, they&apos;ll withstand everything
             the weather can offer.
           </p>
         </div>
